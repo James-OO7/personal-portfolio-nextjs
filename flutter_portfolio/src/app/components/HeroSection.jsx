0076f157be1f10a4f262ba612618af5844cb5fef
James-OO7 <jamesbond03@icloud.com>
2024-02-07 20:27:42 -0500

    Added the Hero Section to start off my project, an d added react-type-animation for hero nav

--- flutter_portfolio/src/app/components/HeroSection.jsx
+++ flutter_portfolio/src/app/components/HeroSection.jsx
@@ -1,19 +1,56 @@
+"use client";
 import React from 'react'
+import Image from 'next/image'
+import { TypeAnimation } from 'react-type-animation';
 
 const HeroSection = () => {
   return( 
   <section>
-      <div className='grid grid-cols-1 lg:grid-cols-12'>
-         <div className='col-span-7 place-self-center'>
-            <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-               Hello, I'm James
+      <div className='grid grid-cols-1 sm:grid-cols-12'>
+         <div className='col-span-7 place-self-center text-center sm:text-left'>
+            <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+               <span className='text-transparent bg-clip-text bg-gradient-to-r from-[#F17B41] to-[#CD4BC9]'>
+                  Hello, I'm {" "}
+               </span>
+               <TypeAnimation
+                  sequence={[
+                  // Same substring at the start will only be typed out once, initially
+                  'James',
+                  1000, // wait 1s before replacing "Mice" with "Hamsters"
+                  'a Software Engineer',
+                  1000,
+                  'a Web Developer',
+                  1000,
+                  'a Full Stack Developer',
+                  1000
+                  ]}
+                  wrapper="span"
+                  speed={50}
+                  repeat={Infinity}
+               />
             </h1>
-            <p className='text-[#ADB7BE] text-lg lg:text-xl'>
+            <p className='text-[#ADB7BE] text-base sm:text-lg mb-7 lg:text-xl'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. 
             </p>
+            <div>
+               <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-[#879AF2] via-[#D3208B] to bg-[#FDA000] hover:bg-slate-200 text-white'>
+                  Hire Me
+               </button>
+               <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-[#879AF2] via-[#D3208B] to bg-[#FDA000] hover:bg-slate-800 text-white mt-3'>
+                  <span className='block hover: bg-slate-800 rounded-full px-5 py-2'>Download Resume</span>
+               </button>
+            </div>
          </div>
-         <div className='col-span-5'>
-
+         <div className='col-span-5 place-self-center mt-28 lg:mt-0'>
+            <div className='rounded-full bg-[#181818] w-[250px] l-[250px] lg:w-[375px] lg:h-[375px] relative'>
+               <Image
+                  src="/images/hero-image(1280).png"
+                  alt='hero image'
+                  className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
+                  width={300}
+                  height={300}
+               />
+            </div>
          </div>
       </div>
   </section>
